2b57ad6af39a749cbd489c59e7f61f86d2c855ea
Alejandro Gonzales <alejandrogonzales@Alejandros-MacBook-Pro.local>
2025-03-05 21:30:19 -0700

    Added prompt function for creating llm prompt with context

--- app/utils/helper_functions.py
+++ app/utils/helper_functions.py
@@ -1,5 +1,7 @@
 # helper functions for chunking LLM text
 
+PROMPT_LIMIT = 3750
+
 # chunk the input text for embedding
 def chunk_text(text, chunk_size=200):
     # split the text by sentences to avoid breaking in the middle of a sentence
@@ -19,3 +21,32 @@ def chunk_text(text, chunk_size=200):
     if current_chunk:
         chunks.append(current_chunk)
     return chunks
+
+# build the prompt using the context to the LLM
+def build_prompt(query, context_chunks):
+    # create start and end of the prompt
+    prompt_start = (
+        "Answer the question based on the context below. If you don't know the answer based on the context provided below, just respond with 'I don't know' instead of making up an answer. Return just the answer to the question, don't add anything else. Don't start your response with the word 'Answer:'. Make sure your response is in markdown format\n\n"+
+        "Context:\n"
+    )
+    prompt_end = (
+        f"\n\nQuestion: {query}\nAnswer:"
+    )
+   
+    # append context chunks until we hit the limit of tokens
+    prompt = ""
+    for i in range(1, len(context_chunks)):
+        if len("\n\n---\n\n".join(context_chunks[:i])) >= PROMPT_LIMIT:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(context_chunks[:i-1]) +
+                prompt_end
+            )
+            break
+        elif i == len(context_chunks)-1:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(context_chunks) +
+                prompt_end
+            )
+    return prompt
